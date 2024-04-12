0c106abb271398cd160fff0616c6d545d54ea471
nouahhabi <noura.ouahhabi@gmail.com>
2024-04-12 18:54:31 +0000

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,42 @@
+import type { Metadata } from "next";
+import { DM_Sans } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/providers/theme-provider";
+import { ClerkProvider } from "@clerk/nextjs";
+import ModalProvider from "@/providers/modal-provider";
+import { Toaster } from "@/components/ui/sonner";
+
+const font = DM_Sans({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Fuzzie",
+  description: "Automate your work with Fuzzie",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
+      <html lang="en">
+        <body className={font.className}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <ModalProvider>
+              {children}
+              <Toaster />
+            </ModalProvider>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
---
+++ src/providers/connections-provider.tsx
@@ -0,0 +1,144 @@
+"use client";
+import { createContext, useContext, useState } from "react";
+
+export type ConnectionProviderProps = {
+  discordNode: {
+    webhookURL: string;
+    content: string;
+    webhookName: string;
+    guildName: string;
+  };
+  setDiscordNode: React.Dispatch<React.SetStateAction<any>>;
+  googleNode: {}[];
+  setGoogleNode: React.Dispatch<React.SetStateAction<any>>;
+  notionNode: {
+    accessToken: string;
+    databaseId: string;
+    workspaceName: string;
+    content: {
+      name: string;
+      kind: string;
+      type: string;
+    };
+  };
+  workflowTemplate: {
+    discord?: string;
+    notion?: {
+      name: string;
+      kind: string;
+      type: string;
+    };
+    slack?: string;
+  };
+  setNotionNode: React.Dispatch<React.SetStateAction<any>>;
+  slackNode: {
+    appId: string;
+    authedUserId: string;
+    authedUserToken: string;
+    slackAccessToken: string;
+    botUserId: string;
+    teamId: string;
+    teamName: string;
+    content: string;
+  };
+  setSlackNode: React.Dispatch<React.SetStateAction<any>>;
+  setWorkFlowTemplate: React.Dispatch<
+    React.SetStateAction<{
+      discord?: string;
+      notion?: {
+        name: string;
+        kind: string;
+        type: string;
+      };
+      slack?: string;
+    }>
+  >;
+  isLoading: boolean;
+  setIsLoading: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+type ConnectionWithChildProps = {
+  children: React.ReactNode;
+};
+
+const InitialValues: ConnectionProviderProps = {
+  discordNode: {
+    webhookURL: "",
+    content: "",
+    webhookName: "",
+    guildName: "",
+  },
+  googleNode: [],
+  notionNode: {
+    accessToken: "",
+    databaseId: "",
+    workspaceName: "",
+    content: {
+      name: "",
+      kind: "",
+      type: "",
+    },
+  },
+  workflowTemplate: {
+    discord: "",
+    notion: {
+      name: "",
+      kind: "",
+      type: "",
+    },
+    slack: "",
+  },
+  slackNode: {
+    appId: "",
+    authedUserId: "",
+    authedUserToken: "",
+    slackAccessToken: "",
+    botUserId: "",
+    teamId: "",
+    teamName: "",
+    content: "",
+  },
+  isLoading: false,
+  setGoogleNode: () => undefined,
+  setDiscordNode: () => undefined,
+  setNotionNode: () => undefined,
+  setSlackNode: () => undefined,
+  setIsLoading: () => undefined,
+  setWorkFlowTemplate: () => undefined,
+};
+
+const ConnectionsContext = createContext(InitialValues);
+const { Provider } = ConnectionsContext;
+
+export const ConnectionsProvider = ({ children }: ConnectionWithChildProps) => {
+  const [discordNode, setDiscordNode] = useState(InitialValues.discordNode);
+  const [googleNode, setGoogleNode] = useState(InitialValues.googleNode);
+  const [notionNode, setNotionNode] = useState(InitialValues.notionNode);
+  const [slackNode, setSlackNode] = useState(InitialValues.slackNode);
+  const [isLoading, setIsLoading] = useState(InitialValues.isLoading);
+  const [workflowTemplate, setWorkFlowTemplate] = useState(
+    InitialValues.workflowTemplate
+  );
+
+  const values = {
+    discordNode,
+    setDiscordNode,
+    googleNode,
+    setGoogleNode,
+    notionNode,
+    setNotionNode,
+    slackNode,
+    setSlackNode,
+    isLoading,
+    setIsLoading,
+    workflowTemplate,
+    setWorkFlowTemplate,
+  };
+
+  return <Provider value={values}>{children}</Provider>;
+};
+
+export const useNodeConnections = () => {
+  const nodeConnection = useContext(ConnectionsContext);
+  return { nodeConnection };
+};
